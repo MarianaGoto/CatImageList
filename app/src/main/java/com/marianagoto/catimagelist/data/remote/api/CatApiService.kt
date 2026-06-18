11ed25e8bb550a07586fe756fff9347cbdac6a71
MarianaGoto <mariana-goto@hotmail.com.br>
2026-06-18 19:16:25 -0300

    chore: adiciona configuração de API key ao projeto

--- app/src/main/java/com/marianagoto/catimagelist/data/remote/api/CatApiService.kt
+++ app/src/main/java/com/marianagoto/catimagelist/data/remote/api/CatApiService.kt
@@ -2,15 +2,18 @@ package com.marianagoto.catimagelist.data.remote.api
 
 import com.marianagoto.catimagelist.domain.model.CatImage
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.Query
 
+const val DEMO_API_KEY = "live_qaRv5a18HnM6T0RvWXscDQZ79JaFJEKWm2kNMOzfmJHAD1evhEMtIeBx9WIfajyW"
 
 interface CatApiService {
     @GET( "images/search")
     suspend fun searchCats(
         @Query("limit") limit: Int = 20,
-        @Query("has_breeds") hasBreeds: Int = 1
+        @Query("has_breeds") hasBreeds: Int = 1,
+        @Header("x-api-key") apiKey: String = DEMO_API_KEY
     ): List<CatImage>
 
     @GET("images/{id}")
